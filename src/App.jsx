13f9b39c4e09570c0a1f34ea9c82546cd1195460
ChandraBose-11 <chandrubose46@gmail.com>
2024-06-13 13:19:08 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,27 @@ import Input from "./Components/Input/Input";
 import Card from "./Components/Card/Card";
 
 const App = () => {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState([
+    {
+      id:1,
+      title:"Guvi Task1",
+      description:"this is the description for my Guvi First task",
+      status:"false"
+    },
+    {
+      id:2,
+      title:"Guvi Task2",
+      description:"this is the description for my Guvi Second task",
+      status:"true"
+    },
+    {
+      id:3,
+      title:"Guvi task",
+      description:"this is the description for my Guvi Third task",
+      status:"false"
+    },
+  ]);
+ 
   const [title, setTitle] = useState("");
   const [des, setDes] = useState("");
   const [status, setStatus] = useState("Not Completed");
